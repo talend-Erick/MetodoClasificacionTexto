c92abe3036617d8430917b33f4888ca6db78744f
ergarciap <erick.garciap01@gmail.com>
2020-07-08 16:49:35 -0600

    Actualiza api

--- api/main.py
+++ api/main.py
@@ -1,12 +1,16 @@
 from typing import List
 
 from fastapi import FastAPI, Query
-
+from pydantic import BaseModel
 from src.models.train import train
 from src.models.predict import predict
 
-app = FastAPI()
+class PredictionSchema(BaseModel):
+    id: int
+    sentence: str
+    prediction: str
 
+app = FastAPI()
 
 @app.post('/train')
 async def train_model():
@@ -14,8 +18,7 @@ async def train_model():
 
     return {'Result': 'model.pkl produced'}
 
-
-@app.get('/predict')
+@app.get('/predict',response_model=List[PredictionSchema])
 async def predict_review(sentences: List[str] = Query(..., description='Sentences to process')):
     predictions = predict(sentences)
 
